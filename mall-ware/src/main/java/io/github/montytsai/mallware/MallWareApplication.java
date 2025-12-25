49934bf607487d8e7ce889378390bd3e430b2592
monty.tsai <montytsai@gmail.com>
2025-12-26 00:40:22 +0800

    feat(project): initialize core microservices modules

---
+++ mall-ware/src/main/java/io/github/montytsai/mallware/MallWareApplication.java
@@ -0,0 +1,13 @@
+package io.github.montytsai.mallware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallWareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallWareApplication.class, args);
+    }
+
+}
